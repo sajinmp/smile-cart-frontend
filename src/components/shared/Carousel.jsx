6cd5351afb89fdc5b728eaabd189bd2591688763
sajinmp <sajin@incubyte.co>
2025-09-15 20:11:58 +0530

    added switching images auto

--- src/components/shared/Carousel.jsx
+++ src/components/shared/Carousel.jsx
@@ -1,15 +1,30 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { Left, Right } from "neetoicons";
 import { Button } from "neetoui";
 
 const Carousel = ({ imageUrls, title }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    timerRef.current = setInterval(handleNext, 3000);
+
+    return () => clearInterval(timerRef.current);
+  });
+
+  const resetTimer = () => {
+    if (timerRef.current) {
+      clearInterval(timerRef.current);
+    }
+    timerRef.current = setInterval(handleNext, 3000);
+  };
 
   const handlePrevious = () => {
     setCurrentIndex(
       currentIndex === 0 ? imageUrls.length - 1 : currentIndex - 1
     );
+    resetTimer();
   };
 
   const handleNext = () => {
@@ -20,24 +35,28 @@ const Carousel = ({ imageUrls, title }) => {
 
   return (
     <div className="flex flex-col items-center">
-      <Button
-        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
-        icon={Left}
-        style="text"
-        onClick={handlePrevious}
-      />
-      <img
-        alt={title}
-        className="max-w-56 h-56 max-h-56 w-56"
-        src={imageUrls[currentIndex]}
-      />
-      <Button
-        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
-        icon={Right}
-        style="text"
-        onClick={handleNext}
-      />
-      <br />
+      <div className="flex items-center space-x-4">
+        <Button
+          className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+          icon={Left}
+          style="text"
+          onClick={handlePrevious}
+        />
+        <img
+          alt={title}
+          className="max-w-56 h-56 max-h-56 w-56"
+          src={imageUrls[currentIndex]}
+        />
+        <Button
+          className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+          icon={Right}
+          style="text"
+          onClick={() => {
+            handleNext();
+            resetTimer();
+          }}
+        />
+      </div>
       <div className="flex space-x-1">
         {imageUrls.map((_, index) => {
           const defaultClasses =
@@ -52,7 +71,10 @@ const Carousel = ({ imageUrls, title }) => {
             <span
               className={dotClassNames}
               key={index}
-              onClick={() => setCurrentIndex(index)}
+              onClick={() => {
+                setCurrentIndex(index);
+                resetTimer();
+              }}
             />
           );
         })}
